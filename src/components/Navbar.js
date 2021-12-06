6ac63d3ca4fe5fb83bde0b34f91995007448e9bc
Casey Carroll <caseydean8@gmail.com>
2021-12-06 11:11:23 -0500

    Added alternate logos and css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import logo from '../logo.png'
+import logo from '../logo3.png'
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="#"><img src={logo} class="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}" alt=""/></a>
+  <a className="navbar-brand" href="#"><img src={logo} className="logo" alt="logo"/></a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
