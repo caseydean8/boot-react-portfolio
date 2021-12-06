27707e3c24b75d9d0c81b5ed5419164f02dc4554
Casey Carroll <caseydean8@gmail.com>
2021-12-06 10:36:59 -0500

    Added logo to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import logo from '../logo.png'
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="#">Navbar</a>
+  <a className="navbar-brand" href="#"><img src={logo} class="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}" alt=""/></a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
@@ -33,13 +34,12 @@ const Navbar = () => {
     </ul>
     <form className="form-inline my-2 my-lg-0">
       <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-      {/* <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      
       <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-       */}
+      
+      
     </form>
 
-      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      {/* <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> */}
       
 </div>
 </nav>
