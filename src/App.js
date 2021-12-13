25e75080ea82727dc374193749a139cfc06d1309
Casey Carroll <caseydean8@gmail.com>
2021-12-13 11:30:29 -0500

    Added header component and background image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
+import Header from './components/Header';
 
 function App() {
   return (
+    <>
     <Navbar />
-  //  <h3>new portfolio</h3> 
+    <Header />
+    < />
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+// shortcut rafce
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="header-wrapper">
+      <div className="main-info">
+        <h1>full stack web development</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Header
