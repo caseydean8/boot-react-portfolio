33826704eef0e132a8c0942f4fca87086f2ac481
Casey Carroll <caseydean8@gmail.com>
2021-12-14 11:22:19 -0500

    Added react-typed package

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,25 @@
 // shortcut rafce
-import React from 'react'
+import React from "react";
+import Typed from "react-typed";
 
 const Header = () => {
   return (
-    <div className="header-wrapper" >
+    <div className="header-wrapper">
       <div className="main-info">
         <h1>full stack web development</h1>
         {/* <svg viewBox="0 0 450 50">
         <text y="50">full stack web development</text>
         </svg> */}
+        <Typed
+          className="typed-text"
+          strings={["full", "stack", "web", "development"]}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
 
 /* <svg viewBox="0 0 450 50">
   <text y="50">Scalable Title</text>
