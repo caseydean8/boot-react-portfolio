0e54b754c99647273f66aafd74e0715f53638a30
Casey Carroll <caseydean8@gmail.com>
2021-12-14 10:52:24 -0500

    Changed image, added text outline to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,19 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className="header-wrapper">
+    <div className="header-wrapper" >
       <div className="main-info">
         <h1>full stack web development</h1>
+        {/* <svg viewBox="0 0 450 50">
+        <text y="50">full stack web development</text>
+        </svg> */}
       </div>
     </div>
   )
 }
 
 export default Header
+
+/* <svg viewBox="0 0 450 50">
+  <text y="50">Scalable Title</text>
+</svg> */
