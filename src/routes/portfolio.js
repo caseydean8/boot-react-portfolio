65ab9e4d38461782274dacbeebdaa50882b9ae1c
Casey Carroll <caseydean8@gmail.com>
2022-01-05 10:31:41 -0500

    Normalized image height in carousel/

--- src/routes/portfolio.js
+++ src/routes/portfolio.js
@@ -1,44 +1,80 @@
 import React from "react";
 import "../App.css";
-import Layout from "../components/Layout"
-import Main from "../components/Main"
-import Navbar from "../components/Navbar"
-
+import Layout from "../components/Layout";
+import Main from "../components/Main";
+import Navbar from "../components/Navbar";
 
 const Portfolio = () => {
   return (
     // <h1>Portfolio</h1>
-    <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-  <ol className="carousel-indicators">
+    <Layout
+      header={<Navbar />}
+      main={
+        <div
+          id="carouselExampleIndicators"
+          className="carousel slide"
+          data-ride="carousel"
+        >
+          {/* <ol className="carousel-indicators">
     <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      <img className="d-block w-60" src="images/ahoy.jpg" alt="First slide"></img>
-    </div>
-    <div className="carousel-item">
-      <img className="d-block w-100" src="images/hippies.jpg" alt="Second slide"></img>
-    </div>
-    <div className="carousel-item">
-      <img className="d-block w-60" src="images/Submarine.png" alt="Third slide"></img>
-    </div>
-  </div>
-  <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="sr-only">Previous</span>
-  </a>
-  <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="sr-only">Next</span>
-  </a>
-</div>
-    // <Layout 
+  </ol> */}
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img
+                className="d-block w-100"
+                src="images/ahoy.jpg"
+                alt="First slide"
+              ></img>
+            </div>
+            <div className="carousel-item">
+              <img
+                className="d-block w-100"
+                src="images/hippies.jpg"
+                alt="Second slide"
+              ></img>
+            </div>
+            <div className="carousel-item">
+              <img
+                className="d-block w-100"
+                src="images/Submarine.png"
+                alt="Third slide"
+              ></img>
+            </div>
+          </div>
+          <a
+            className="carousel-control-prev"
+            href="#carouselExampleIndicators"
+            role="button"
+            data-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="sr-only">Previous</span>
+          </a>
+          <a
+            className="carousel-control-next"
+            href="#carouselExampleIndicators"
+            role="button"
+            data-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="sr-only">Next</span>
+          </a>
+        </div>
+      }
+    />
+    // <Layout
     // header={<Navbar />}
     // main={<h1>Portfolio</h1>}
     // />
-  )
-}
+  );
+};
 
 export default Portfolio;
