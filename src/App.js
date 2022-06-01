6c4e1825ced4bb442b5d89ac58c98355192ae767
Casey Carroll <caseydean8@gmail.com>
2022-06-01 19:38:21 -0400

    Moved Hashrouter to  index.js for github deploy.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./routes/Home";
 import About from "./routes/About";
@@ -8,14 +8,14 @@ import Contact from "./routes/Contact";
 function App() {
   return (
     <div className="container-fluid">
-      <HashRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-        </Routes>
-      </HashRouter>
+      {/* <HashRouter> */}
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+      {/* </HashRouter> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDom from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
