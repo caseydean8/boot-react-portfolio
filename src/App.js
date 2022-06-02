2f3faea022fa9138c8ccad680fe4b60308a4a3fd
Casey Carroll <caseydean8@gmail.com>
2022-06-01 20:31:24 -0400

    Changed routing method for deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./routes/Home";
 import About from "./routes/About";
@@ -8,14 +8,14 @@ import Contact from "./routes/Contact";
 function App() {
   return (
     <div className="container-fluid">
-      {/* <HashRouter> */}
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
-      </Routes>
-      {/* </HashRouter> */}
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDom from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+// import { HashRouter } from "react-router-dom";
 import App from "./App";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
-    <HashRouter>
+  // <React.StrictMode>
+    // <HashRouter>
       <App />
-    </HashRouter>
-  </React.StrictMode>
+    // </HashRouter>
+  // </React.StrictMode>
 );
