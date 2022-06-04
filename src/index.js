0bb75ee4d38e75dd4f0843ef1922fc342aa56c33
Casey Carroll <caseydean8@gmail.com>
2022-06-04 15:51:12 -0400

    Cleaned up index.js

--- src/index.js
+++ src/index.js
@@ -8,10 +8,6 @@ const root = ReactDom.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
-      <App />
-=======
     <App />
->>>>>>> c62e996e62acb985ef22647e4bcc50f01517ba17
   </React.StrictMode>
 );
