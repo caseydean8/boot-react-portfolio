d4f0b36db0e47d980b6f56025503469603ed842e
Casey Carroll <caseydean8@gmail.com>
2022-10-17 10:06:18 -0400

    Text edits

--- src/routes/about.js
+++ src/routes/about.js
@@ -6,9 +6,7 @@ export default function About() {
       <h1 className="contact-header">about</h1>
       <p>
         I build dynamic user experiences and robust applications utilizing cutting edge technologies and
-        frameworks as well as classic web architecture to bring
-        software ideas to life. Solving problems is my passion, but in my free time I enjoy gardening and hiking with
-        my family in Northeast PA.
+        frameworks as well as classic web architecture to bring software ideas to life. Solving problems is my passion but in my free time I enjoy gardening and hiking with my family in Northeast PA.
       </p>
     </div>
   );
