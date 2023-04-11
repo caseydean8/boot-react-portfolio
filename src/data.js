944c2096c37cca4f7b97d09e8728d11c8bc278a7
Casey Carroll <caseydean8@gmail.com>
2023-04-11 13:57:27 -0400

    Edit project descriptions

--- src/data.js
+++ src/data.js
@@ -16,21 +16,21 @@ const data = [
       "Submarine is a subscription tracking app built with the react, node, mongo, express, and bootstrap. \n You can take a look around with username 'guest' and password 'guest'.",
   },
   {
+    id: "honesdale-cc",
+    image: HonesdaleCC,
+    link: "https://honesdale-cc-next.vercel.app/",
+    title: "Honesdale Community Church",
+    repo: "https://github.com/caseydean8/honesdale-cc-next",
+    text: "Website of the Honesdale Community Church that utilizes Hygraph's CMS to allow church admins to update the event page. Built with Next.js and Bootstrap.",
+  },
+  {
     id: "shopping-app",
     image: ShoppingApp,
     link: "https://shopity.herokuapp.com/",
     title: "Shopity",
     repo: "https://github.com/caseydean8/shopity-app",
     text:
-      "Shopity is helpful shopping assistant that allows users to login and create a list of pantry items, which can then moved onto a shopping list and then into a virtual cart. Shopity was created with the Pug template engine, an SQL database with the sequelize.js ORM, and express",
-  },
-  {
-    id: "honesdale-cc",
-    image: HonesdaleCC,
-    link: "https://honesdale-cc-next.vercel.app/",
-    title: "Honesdale Community Church",
-    repo: "https://github.com/caseydean8/honesdale-cc-next",
-    text: "This site is the built with Next.js and Hygraph. The Hygraph CMS to allow church admins to make updates to the events pages. Email.js is utilized for the contact form.",
+      "Shopity is helpful shopping assistant that allows users to login and create a list of pantry items, which can then be added to an active shopping list and finally a virtual cart. Shopity was created with the Pug, SQL, and express.",
   },
   {
     id: "graphql-mgmt",
@@ -39,7 +39,7 @@ const data = [
     title: "GraphQL MGMT",
     repo: "https://github.com/caseydean8/GraphQL-MGMT-App",
     text:
-      "GraphQL MGMT is a full stack application using the GraphQL query management system. It is built with React, Express, MongoDB, Apollo, and Bootstrap 5. Users can create, update, and delete clients as well as their related projects",
+      "GraphQL MGMT is a full stack application using the GraphQL query management system. Users can create, update, and delete clients as well as their related projects. Built with React, Express, MongoDB, Apollo, and Bootstrap 5. ",
   },
   {
     id: "book-safe",
@@ -48,7 +48,7 @@ const data = [
     title: "Book Safe",
     repo: "https://github.com/caseydean8/book-safe",
     text:
-      "Book Safe is an full stack MERN application that allows the user to search for a book title and receive information, including the books author or authors, a thumbnail of the book cover, and a link to the book on google books. The user can then save the book information on a remote database for future reference, and delete the book from that database if desired.\n \n The application utilizes the following technologies: \n React \n Node \n Express \n MongoDB \n Heroku \n axios \n bootstrap",
+      "Book Safe is a full stack MERN application that allows the user to search for a book title and retrieve information including the author, a thumbnail of the cover, and a link to google books. The user can then save that information for future reference.\n \n The application utilizes the following technologies: \n React \n Node \n Express \n MongoDB \n Heroku \n axios \n bootstrap",
   },
   {
     id: "crystal-vibes",
@@ -57,7 +57,7 @@ const data = [
     title: "Crystal Vibes",
     repo: "https://github.com/caseydean8/crystal-vibes",
     text:
-      "This project uses javascript and jQuery to render a number guessing game. \n A random number between 19 and 120 is displayed, and a random number between 1 and 12 is assigned to each of four crystal buttons but is not displayed. \n When a player clicks on a button, the value is added to a score below the buttons and if the player can match the random number displayed, a win is recorded. If the player goes over a loss is recorded. \n Button values are displayed in the console for testing.",
+      "This app utilizes jQuery to render a number guessing game. \n A random number is displayed, and each of four crystal buttons is assigned a secret value. \n Click a button and that value is added to your score. If you can match the target number, praise an adulation commence. If not . .  .",
   },
   // {
   //   id: "",
